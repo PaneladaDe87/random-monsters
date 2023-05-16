e9f571b52a6c44375b623c739464a378c562abbe
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-16 00:36:13 -0300

    Create main.asm

---
+++ source/main.asm
@@ -0,0 +1,34 @@
+SECTION "Variables", VRAMO
+random_number: DB 0
+guess:         DB 0
+
+SECTION "Code", ROMO
+
+start:
+    ld   a, 10
+    call randomize
+    ld   (random_number)
+    
+prompt:
+    ld hl, prompt_msg
+    call prompt_string
+    
+    read_input:
+    ld hl, guess
+    call read_digit
+    
+    ld a, (guess)
+    cp (random_number)
+    jr z, correct_guess
+    jr prompt
+    
+    correct_guess:
+    ld hl, correct_msg
+    call print_string
+    
+randomize:
+    ld a, r
+    xor $1F
+    add $01
+    ret
+    
