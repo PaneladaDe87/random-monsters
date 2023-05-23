aa91afe871e6001ec23bbd3b1cbb760bf23d50cf
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-23 16:18:56 -0300

    Update game.asm

--- game.asm
+++ game.asm
@@ -2,11 +2,32 @@ ORG 100h
 
 START:
     LD A, 1
-    CALL LOADING ; chame a subrotina LOADING
+    CALL LOADING     ; chame a subrotina LOADING
     
-    JMP START    ; volta para a rotina principal
+    JMP START        ; volta para a rotina principal
+    
+CONDICTION:
+    JP LOL           ; para evitar bugs e executar tudo certo
     
 LOADING:
-    ADD A, 1     ; adiciona mais 1 ao registrador A
+    ADD A, 1         ; adiciona mais 1 ao registrador A
     
-    RET          ; retorna o ponto de chamada(START) para fazer a porra do loop de novo
+    RET              ; retorna o ponto de chamada(START) para fazer a porra do loop de novo
+
+LOL:
+    LD HL, MESSAGE   ; mensagem a ser imprimida
+    CALL PRINT       ; ooooooooooomaaaagawd
+    
+PRINT:
+    LD A, (HL)       ; a zoeira não para lol
+    OR A             ; hehehehaw
+    JP Z, END        ; fim
+    
+    INC HL
+    JP IMPRIMIR
+    
+END:
+    RET              ; coolswag
+    
+MESSAGE:
+    DB "AMOGUS", 0   ; termina com 0 porque sim
