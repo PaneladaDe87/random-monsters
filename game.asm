c302d68a533bb7aee08177f946d7498643a2ac0f
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-23 20:40:21 -0300

    Update game.asm

--- game.asm
+++ game.asm
@@ -1,14 +1,26 @@
 ORG 150h
+
 LD A, $80
 LD ($FF40), A
 
-LD HL, $8000
-LD DE, $FFFF
+LD HL, TEXT
+LD DE, $8000
 
-LD BC, $4000
-LDIR
+CALL PRINT_STRING
 
 MAIN_LOOP:
     JR MAIN_LOOP
-    
-    END
+
+PRINT_STRING:
+    LD A, (HL)
+    OR A
+    RET Z
+    LD ($8000), A
+    INC HL
+    INC DE
+    JR PRINT_STRING
+
+TEXT:
+  DB 'this game is brazilian', 0
+  
+END
