8c2e86b13ce425d4387505833c8e37ae54afa1d4
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-24 13:23:12 -0300

    Update game.asm

--- game.asm
+++ game.asm
@@ -3,24 +3,19 @@ ORG 150h
 LD A, $80
 LD ($FF40), A
 
-LD HL, TEXT
-LD DE, $8000
+LD HL, $9800  ; Endereço da tela de exibição (vide RAM do Game Boy)
+LD BC, $2000  ; Tamanho total da tela de exibição (32x32 tiles)
+LD A, $FF     ; Cor branca
 
-CALL PRINT_STRING
+CLEAR_SCREEN_LOOP:
+  LD (HL), A   ; Define o valor da cor branca no endereço da tela
+  INC HL       ; Avança para o próximo byte na memória da tela
+  DEC BC       ; Decrementa o contador de bytes restantes
+  LD A, B
+  OR C
+  JR NZ, CLEAR_SCREEN_LOOP  ; Repete o loop enquanto ainda houverem bytes restantes
 
 MAIN_LOOP:
-    JR MAIN_LOOP
+  JR MAIN_LOOP
 
-PRINT_STRING:
-    LD A, (HL)
-    OR A
-    RET Z
-    LD ($8000), A
-    INC HL
-    INC DE
-    JR PRINT_STRING
-
-TEXT:
-  DB 'this game is brazilian', 0
-  
 END
