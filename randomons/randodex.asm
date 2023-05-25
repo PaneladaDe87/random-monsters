40179ec50d30103cf16a8e43a01314e4a5d9e672
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-25 19:11:16 -0300

    Update and rename shinto.asm to randodex.asm

---
+++ randomons/randodex.asm
@@ -0,0 +1,25 @@
+INFO_SHINTO:
+    name        DB "Shinto"
+    type        DB "Ground"
+    height      DB "1.25 meters"
+    weight      DB "5 kilo"
+    gender      DB "Male"
+    effect      DB "None"
+    status      DB "Catched"
+    nature      DB "Domestic"
+    
+INFO_SHITNO:
+    name        DB "Shitno"
+    type_1      DB "Ground"
+    height      DB "2.1 meters"
+    weight      DB "90 kilo"
+    gender      DB "Male"
+    effect      DB "None"
+    status      DB "Uncatched"
+    nature      DB "Wild"
+    
+CHECK_KEY:
+    exit        DB "A"
+    description DB "B"
+    nav_down    DB "Down"
+    nav_up      DB "Up"
--- randomons/shinto.asm
+++
@@ -1,7 +0,0 @@
-INFO:
-  name   DB    "Shinto"
-  type   DB    "Ground"
-  height DB    "1.25 meters"
-  weight DB    "5 kilo"
-  gender DB    "Male"
-  effect DB    "None"
