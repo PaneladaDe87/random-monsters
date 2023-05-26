5b562a6e57baa1b005a7e1958bc7b3a6ffd04f04
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-26 14:03:26 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,5 +1,14 @@
 ORG 150h
-LD A, $FF
 
-MAIN_LOOP:
-    LD A, $75
+LD A, $1
+press EQU "A"
+
+MAIN:
+    start EQU $0
+    CP 0
+    JR Z, NUMBER_ONE
+    
+    JR MAIN
+    
+NUMBER_ONE:
+    LD A, $2
