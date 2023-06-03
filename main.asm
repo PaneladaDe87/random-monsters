291d6ab754d8e6cfe8f2b6de20f646a6fb2c4a37
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-06-03 17:23:26 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -4,9 +4,23 @@ LD A, $0
 
 MAIN:
     CP 0
-    JR Z, NUMBER_ONE
+    JR Z, START
     
     JR MAIN
+
+START:
+    button_a EQU 0
+    button_b EQU 1
     
-NUMBER_ONE:
-    LD A, $1
+    WAIT_FOR_BUTTON:
+        IN A
+        
+        AND %00000011
+        CP %00000001
+        
+        JR Z, INTRO
+        
+        JR WAIT_FOR_BUTTON
+        
+    INTRO:
+        LD A, 0
