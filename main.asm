0087acef7d38483ace72ca5b62d5582d300ae632
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-06-19 21:49:11 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -3,6 +3,24 @@ ORG 150h
 LD A, $0
 ADD A, 1
 
+NOTE_A = 5
+NOTE_B = 6
+NOTE_C = 0
+NOTE_D = 1
+NOTE_E = 2
+NOTE_F = 3
+NOTE_G = 4
+BEATS_PER_MINUTE = 100
+
+MUSIC_DATA:
+    DB NOTE_A
+    DB NOTE_B
+    DB NOTE_C
+    DB NOTE_E
+    DB NOTE_F
+    DB NOTE_G
+    DB 0
+
 MAIN:
     CP 0
     JR Z, START
