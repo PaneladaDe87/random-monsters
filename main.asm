53af7a641622411aef299f76f2c55aa88034af89
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-10 00:30:19 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -3,19 +3,10 @@ ORG 0x0000
 JP START
 
 START:
-    LD A, (0)
-
-    JP LOADING
-
-    LOADING:
-        ADD A, (1)
-        CP A
-        
-        JR NZ, MENU
-        JR Z, LOADING
+    JP MENU
 
     MENU:
-        LD A, (0)
+        LD A, 0
 
         DB "press START button to start"
 
@@ -24,26 +15,26 @@ START:
         JR NZ, PLAY
 
     PLAY:
-        LD C, (0)
+        LD C, 0
         CP D
         JR Z, PLAY
         JR NZ, WALK_RIGHT
 
 WALK_RIGHT:
     ; move one tile to forward
-    ADD C, (1)
+    ADD A, 1
 
 WALK_LEFT:
     ; move one tile to behind
-    SUB C, (1)
+    SUB A, 1
 
 WALK_UP:
     ; move one tile to up
-    ADD D, (1)
+    ADD A, 1
 
 WALK_DOWN:
     ; move one tile to down
-    SUB D, (1)
+    SUB 1, 1
 
 CHECK_BUTTON_PRESSED:
     LD C, 0
