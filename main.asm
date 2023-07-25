6bf18e46d29a0f5302c19b76285140e26998c097
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-24 22:52:24 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,13 +1,13 @@
 ORG 0x0000
 
+; The script starts here
+
 JP START
 
 START:
     JP MENU
 
     MENU:
-        LD A, 0
-
         DB "press START button to start"
 
         CP C
@@ -22,20 +22,26 @@ START:
         JR Z, PLAY
         JR NZ, CHECK_INPUT
 
+; Walking logic
+
 WALK_LEFT:
     LD B, 1
+    JR CHECK_INPUT
 
 WALK_RIGHT:
     LD B, 2
+    JR CHECK_INPUT
 
 WALK_UP:
     LD B, 3
+    JR CHECK_INPUT
 
 WALK_DOWN:
     LD B, 4
+    JR CHECK_INPUT
 
 IDLE:
-    LD B, 0
+    RET
 
 ; Check a button if pressed
 ; The player can move in 4 directions
@@ -44,7 +50,56 @@ CHECK_INPUT:
     CP D
     JR Z, CHECK_INPUT
 
-    ; If the register D is one
-    CALL WALK_LEFT
+    ; If D is 1
+    CP 1
+    JR Z, WALK_LEFT
 
-    ; I will stop here, for now
+    ; If D is 2
+    CP 2
+    JR Z, WALK_RIGHT
+
+    ; If D is 3
+    CP 3
+    JR Z, WALK_UP
+
+    ; If D is 4
+    CP 4
+    JR Z, WALK_DOWN
+
+    ; If D is 0
+    CALL IDLE
+    JR CHECK_INPUT
+
+; Randomons logic(battle, encounter and catch)
+
+; Logic to found a randomon
+RANDOMON_ENCOUNTER:
+    CP H
+    JR Z, RANDOMON_ENCOUNTER
+
+    CP 1
+    JR Z, RANDOMON_BATTLE
+
+; Logic to the founded randomon
+RANDOMON_BATTLE:
+    CP E
+    JR Z, RANDOMON_BATTLE
+
+    CP 1
+    JR Z, SHINTO
+
+    ; Shinto battle logic
+    SHINTO:
+        LD A, 5
+
+        CP A
+        JR Z, SHINTO
+
+        CP 5
+        DB "Shinto health is full"
+
+        CP 3
+        DB "Shinto health is half"
+
+        CP 0
+        DB "Shinto health is empty"
