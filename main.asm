57737770fdd6011691f67926979d88c5c740be25
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-30 19:39:24 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -104,9 +104,9 @@ RANDOMON_BATTLE:
             ; The RAG power is like the elixir from Clash Royale
             ; Each randomon can have 10 RAG power or lower
 
-            LD A, 10
+            LD A', 10
 
-            CP A
+            CP A'
             JR Z, SHINTO_RAG_POWER
 
             CP 10
